00371a9eb495f1999038d66242e261f66911b04a
Aliaksandr Martsavy - CW <aliaksandr.martsavy@cfainstitute.org>
2024-01-18 10:25:40 +0100

    Add lost variable.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,3 +9,9 @@ variable "location" {
   description = "The Azure location where all resources in this example should be created"
   default     = "East US"
 }
+
+variable "image" {
+  type        = string
+  description = "Full name with tag of the docker image in the public docker hub"
+  default     = "amartsavy/my-sh-app:1.0"
+}
